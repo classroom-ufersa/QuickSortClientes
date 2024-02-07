1ab36efb7bcd72cd49082a61e2a67a79b2b64568
Allan Freitas <allangabrieldev@gmail.com>
2024-02-07 12:29:26 -0300

    adicionei o tipo estuturado Cliente e fiz update em clente.h

---
+++ cliente.c
@@ -0,0 +1,6 @@
+#include "cliente.h"
+struct cliente{
+    char nome[20];
+    char endereço[30];
+    int codigo_cliente;
+};
---
+++ cliente.h
@@ -0,0 +1,3 @@
+/*Renomeei o tipo estruturado cliente que contêm todos os dados dos clientes cadastrados: nome, endereço e o código
+de verificação do cliente*/
+typedef struct cliente Cliente;
