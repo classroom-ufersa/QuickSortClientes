864b16fb96eb2c28b8f585eab15e250204c58eb2
clara leal <mariiaclaraleal@gmail.com>
2024-02-21 12:24:23 -0300

    clientes serao adicionados sempre que rodar o codigo

--- cliente.c
+++ cliente.c
@@ -8,7 +8,7 @@ struct cliente{ //Criação do meu tipo estruturado clientes
 };
 
 void atribuirDados(Cliente *clientes, int n){
-    FILE *client = fopen("clientes.txt", "wrt"); //Criação do arquivo clientes.txt
+    FILE *client = fopen("clientes.txt", "a"); //Criação do arquivo clientes.txt
     for(int i = 0; i < n; i++){
         fprintf(client, "código do cliente: %d\t Nome: %s\t Endereço: %s\n", clientes[i].codigo_cliente,
         clientes[i].nome, clientes[i].endereco); //Cadastro no arquivo txt do código, nome e endereço
